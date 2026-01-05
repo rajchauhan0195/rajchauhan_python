17b1dcf26c5e3d6789450306bc335158c040fd41
rajchauhan0195 <rajchauhan001957@gmail.com>
2026-01-05 10:44:21 +0530

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,31 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello word")
+hello word
+>>> x=20
+>>> print(x)
+20
+>>> x=20.5
+>>> print(x)
+20.5
+>>> x=1j
+>>> print(x)
+1j
+>>> x=["apple","banana","cherry"]
+>>> print(x)
+['apple', 'banana', 'cherry']
+>>> x=[("apple","banana","cherry")]
+>>> print(x)
+[('apple', 'banana', 'cherry')]
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined
+>>> x=True
+>>> print(x)
+True
+>>> x={"name":"Jhon","age":"36"}
+>>> print(x)
+{'name': 'Jhon', 'age': '36'}
+>>> 
