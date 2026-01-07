927bc56a22d04bacb4ede99d242f901b8025116f
rajchauhan0195 <rajchauhan001957@gmail.com>
2026-01-07 08:56:42 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,62 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x=10
+print(x)
+10
+a=5
+print(-a)
+-5
+p=10
+q=5
+print(p>q)
+True
+print(p==q)
+False
+x=true
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x=True
+y=False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> a=5
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a|b)
+7
+>>> number=[1,2,3,4,5,6]
+>>> print(2 in number)
+True
+>>> print(5not in number)
+False
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> rint(x is not y)
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    rint(x is not y)
+NameError: name 'rint' is not defined. Did you mean: 'print'?
+>>> print(x is not y)
+False
+>>> a=10
+>>> b=5
+>>> print(a+b)
+15
+>>> print(a-b)
+5
+>>> print(a*b)
+50
+>>> print(a/b)
+2.0
